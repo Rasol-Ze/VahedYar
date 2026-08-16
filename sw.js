69da054f94b7d4acefc9311bd776a442380e5832
Amigo <rasol.zarei83@icloud.com>
2026-08-16 15:00:07 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const params = new URLSearchParams(location.search);
-const APP_VERSION = params.get('v') || '1.0.0';
+const APP_VERSION = '1.0.2';
 
 const CACHE_NAME = `vahedyar-v${APP_VERSION}`;
 
